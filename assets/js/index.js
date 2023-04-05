038908cc0c3806444779c46d14da7c97b327d0ac
ayoung <ay9318@naver.com>
2023-04-05 22:04:23 +0900

    인스타그램 스타일로 변경

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,5 @@
-let btnMode = document.querySelector('.btn_mode');
-let portfolio = document.querySelector('.portfolio');
-let mstory = document.querySelector('.port_box.mstory');
-let modal = document.querySelector('.modal_wrap');
-let modalButton = document.querySelector('.modal_wrap button');
-let modalDim = document.querySelector('.modal_wrap .dim');
+let feedItem = document.querySelectorAll('.feed_item');
+let feedItemLength = feedItem.length;
+let lengthBox = document.querySelector('.text_wrap .length');
 
-btnMode.addEventListener('click', function() {
-    if(document.querySelector('.content_wrap').classList.contains('dark')) {
-        document.querySelector('.content_wrap').classList.remove('dark');
-    } else {
-        document.querySelector('.content_wrap').classList.add('dark');
-    }
-});
-
-window.addEventListener('scroll', function(){
-    let scrollTop = window.scrollY;
-    let portTop = portfolio.getBoundingClientRect().top;
-    if(scrollTop >= portTop) {
-        portfolio.classList.add('ani');
-    } else {
-        portfolio.classList.remove('ani');
-    }
-})
-
-mstory.addEventListener('click', function(){
-    modal.style.display = 'block';
-})
-modalDim.addEventListener('click', function(){
-    modal.style.display = 'none';
-})
-modalButton.addEventListener('click', function(){
-    modal.style.display = 'none';
-})
+lengthBox.innerHTML = feedItemLength;
